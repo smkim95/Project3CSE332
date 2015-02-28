98ae5311b19f5fb5b7a9d7543831ebd8a64740d1
smkim95 <seokmin.kim95@gmail.com>
2015-02-27 16:31:42 -0800

    version 1 rougly scatched. Still have some erros to fix.
    I am not sure when to use Pair.java and Rectangle.java. But I think this
    is what we are supposed to do.

--- src/PopulationQuery.java
+++ src/PopulationQuery.java
@@ -1,55 +1,58 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PopulationQuery {
 	// next four constants are relevant to parsing
-	public static final int TOKENS_PER_LINE  = 7;
+	public static final int TOKENS_PER_LINE = 7;
 	public static final int POPULATION_INDEX = 4; // zero-based indices
-	public static final int LATITUDE_INDEX   = 5;
-	public static final int LONGITUDE_INDEX  = 6;
-	
+	public static final int LATITUDE_INDEX = 5;
+	public static final int LONGITUDE_INDEX = 6;
+
 	// parse the input file into a large array held in a CensusData object
 	public static CensusData parse(String filename) {
 		CensusData result = new CensusData();
-		
-        try {
-            BufferedReader fileIn = new BufferedReader(new FileReader(filename));
-            
-            // Skip the first line of the file
-            // After that each line has 7 comma-separated numbers (see constants above)
-            // We want to skip the first 4, the 5th is the population (an int)
-            // and the 6th and 7th are latitude and longitude (floats)
-            // If the population is 0, then the line has latitude and longitude of +.,-.
-            // which cannot be parsed as floats, so that's a special case
-            //   (we could fix this, but noisy data is a fact of life, more fun
-            //    to process the real data as provided by the government)
-            
-            String oneLine = fileIn.readLine(); // skip the first line
 
-            // read each subsequent line and add relevant data to a big array
-            while ((oneLine = fileIn.readLine()) != null) {
-                String[] tokens = oneLine.split(",");
-                if(tokens.length != TOKENS_PER_LINE)
-                	throw new NumberFormatException();
-                int population = Integer.parseInt(tokens[POPULATION_INDEX]);
-                if(population != 0)
-                	result.add(population,
-                			   Float.parseFloat(tokens[LATITUDE_INDEX]),
-                		       Float.parseFloat(tokens[LONGITUDE_INDEX]));
-            }
+		try {
+			BufferedReader fileIn = new BufferedReader(new FileReader(filename));
 
-            fileIn.close();
-        } catch(IOException ioe) {
-            System.err.println("Error opening/reading/writing input or output file.");
-            System.exit(1);
-        } catch(NumberFormatException nfe) {
-            System.err.println(nfe.toString());
-            System.err.println("Error in file format");
-            System.exit(1);
-        }
-        return result;
+			// Skip the first line of the file
+			// After that each line has 7 comma-separated numbers (see constants
+			// above)
+			// We want to skip the first 4, the 5th is the population (an int)
+			// and the 6th and 7th are latitude and longitude (floats)
+			// If the population is 0, then the line has latitude and longitude
+			// of +.,-.
+			// which cannot be parsed as floats, so that's a special case
+			// (we could fix this, but noisy data is a fact of life, more fun
+			// to process the real data as provided by the government)
+
+			String oneLine = fileIn.readLine(); // skip the first line
+
+			// read each subsequent line and add relevant data to a big array
+			while ((oneLine = fileIn.readLine()) != null) {
+				String[] tokens = oneLine.split(",");
+				if (tokens.length != TOKENS_PER_LINE)
+					throw new NumberFormatException();
+				int population = Integer.parseInt(tokens[POPULATION_INDEX]);
+				if (population != 0)
+					result.add(population,
+							Float.parseFloat(tokens[LATITUDE_INDEX]),
+							Float.parseFloat(tokens[LONGITUDE_INDEX]));
+			}
+
+			fileIn.close();
+		} catch (IOException ioe) {
+			System.err
+					.println("Error opening/reading/writing input or output file.");
+			System.exit(1);
+		} catch (NumberFormatException nfe) {
+			System.err.println(nfe.toString());
+			System.err.println("Error in file format");
+			System.exit(1);
+		}
+		return result;
 	}
 
 	// argument 1: file name for input data: pass this to parse
@@ -58,7 +61,71 @@ public class PopulationQuery {
 	// argument 4: -v1, -v2, -v3, -v4, or -v5
 	public static void main(String[] args) {
 		// FOR YOU
-		// checking the updates
+		String fileName = "src/" + args[0];
+		int xDimension = Integer.parseInt(args[1]);
+		int yDimension = Integer.parseInt(args[2]);
+		String version = args[3];
+
+		// needs switch statements
+		// what if file contains no data?
+		CensusData listOfCensusGroup = parse(fileName);
+		// find the minimum and maximum latitude and longitude for mapping
+		float latMax = listOfCensusGroup.data[0].latitude;
+		float longMax = listOfCensusGroup.data[0].longitude;
+		float latMin = listOfCensusGroup.data[0].latitude;
+		float longMin = listOfCensusGroup.data[0].longitude;
+		long totalPopulation = 0;
+
+		for (int i = 0; i < listOfCensusGroup.data_size; i++) {
+			if (latMax < listOfCensusGroup.data[i].latitude) {
+				latMax = listOfCensusGroup.data[i].latitude;
+			}
+			if (longMax < listOfCensusGroup.data[i].longitude) {
+				longMax = listOfCensusGroup.data[i].longitude;
+			}
+			if (latMin > listOfCensusGroup.data[i].latitude) {
+				latMin = listOfCensusGroup.data[i].latitude;
+			}
+			if (longMin > listOfCensusGroup.data[i].longitude) {
+				longMin = listOfCensusGroup.data[i].longitude;
+			}
+			totalPopulation += listOfCensusGroup.data[i].population;
+		}
+
+		// gets input from the user. may be declare scanner as final in the
+		// class variable?
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please give west, south, east, north coordinates of your query rectangle: ");
+		// maybe use pair here
+		// may need exception handler when these values are not correct
+		// putting -1 because user we are using 0 indexing.
+		int west = input.nextInt() - 1;
+		int south = input.nextInt() - 1;
+		int east = input.nextInt() - 1;
+		int north = input.nextInt() - 1;
+
+		float latScale = (latMax - latMin) / yDimension;
+		float longScale = (longMax - longMin) / xDimension;
+
+		// not sure where to use Rectangle or encompass
+		// Rectangle rect = new Rectangle(west, east, north, south);
+
+		int localPopulation = 0;
+
+		for (int i = 0; i < listOfCensusGroup.data_size; i++) {
+			if (west * longScale + longMin <= listOfCensusGroup.data[i].longitude
+					&& east * longScale + longMin >= listOfCensusGroup.data[i].longitude
+					&& south * latScale + latMin <= listOfCensusGroup.data[i].latitude
+					&& north * latScale + latMin >= listOfCensusGroup.data[i].latitude) {
+				localPopulation += listOfCensusGroup.data[i].population;
+			}
+		}
+
+		System.out.println("population of rectangle: " + localPopulation);
+		System.out.println("percent of total population: "
+				+ (double) localPopulation / totalPopulation * 100);
 		
+		//loop?
 	}
 }
